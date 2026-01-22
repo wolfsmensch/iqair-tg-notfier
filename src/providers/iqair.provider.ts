e94bf8779e1d0018c8f82c82baebde27a727a4fb
Eugene Trotsenko <tiberian.boy@protonmail.com>
2026-01-22 23:33:43 +0500

    Для класса IqAirProvider добавлены модификаторы доступа

--- src/providers/iqair.provider.ts
+++ src/providers/iqair.provider.ts
@@ -4,19 +4,19 @@ export class IqAirProvider {
     /**
      * CSS селектор блока с баллами IQ Air на странице города
      */
-    protected static readonly indexValueCssSelector = '#main-content [class*="aqi-box-shadow-"] p.text-lg.font-medium';
+    private static readonly indexValueCssSelector = '#main-content [class*="aqi-box-shadow-"] p.text-lg.font-medium';
 
     /**
      * Значение User-Agent для запроса HTML страницы
      */
-    protected static readonly requestUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
+    private static readonly requestUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
 
     /**
      * Получить баллы IQ Air по URL города
      * @param cityUrl URL города
      * @returns Баллы IQ Air
      */
-    async getIndexByCityUrl(cityUrl: string): Promise<number> {
+    public async getIndexByCityUrl(cityUrl: string): Promise<number> {
         return this._getIndexFromCityPageHtml(
             await this._getCityPageHtml(cityUrl)
         );
@@ -27,7 +27,7 @@ export class IqAirProvider {
      * @param cityUrl URL страницы города
      * @returns HTML страницы города
      */
-    async _getCityPageHtml(cityUrl: string): Promise<string> {
+    private async _getCityPageHtml(cityUrl: string): Promise<string> {
         const response = await fetch(cityUrl, {
             headers: {
                 'User-Agent': IqAirProvider.requestUserAgent,
@@ -44,7 +44,7 @@ export class IqAirProvider {
      * @param pageHtml HTML страницы города
      * @returns {number} Баллы IQ Air
      */
-    _getIndexFromCityPageHtml(pageHtml: string): number {
+    private _getIndexFromCityPageHtml(pageHtml: string): number {
         const $ = cheerio.load(pageHtml);
         const indexValueNode = $(IqAirProvider.indexValueCssSelector);
         
